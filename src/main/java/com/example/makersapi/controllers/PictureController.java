8cbf68f12ec4ed31bef2bf99b9fd3a996bc05a4e
Jorge Flores <jafp07@gmail.com>
2022-07-01 01:50:03 -0500

    added files

---
+++ src/main/java/com/example/makersapi/controllers/PictureController.java
@@ -0,0 +1,36 @@
+package com.example.makersapi.controllers;
+
+import com.example.makersapi.domain.Picture;
+import com.example.makersapi.repositories.PictureRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class PictureController {
+    private final PictureRepository pictureRepository;
+
+    public PictureController(PictureRepository pictureRepository) {
+        this.pictureRepository = pictureRepository;
+    }
+
+    @CrossOrigin
+    @GetMapping("/pictures/{start}")
+    public Page<Picture> getPictures(@PathVariable int start) {
+        Pageable range = PageRequest.of(start, 10);
+        return pictureRepository.findAll(range);
+    }
+
+    @CrossOrigin
+    @GetMapping("/pictures2")
+    public List<Picture> getPictures2() {
+        return pictureRepository.findAll();
+    }
+}
---
+++ src/test/java/com/example/makersapi/MakersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.makersapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakersApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
