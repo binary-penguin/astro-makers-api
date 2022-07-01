14b66b8a00c02fc55854a515236ce70b9ee1ccb5
Jorge Flores <jafp07@gmail.com>
2022-07-01 02:23:43 -0500

    added init

--- src/main/java/com/example/makersapi/repositories/PictureRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.makersapi.repositories;
-
-import com.example.makersapi.domain.Picture;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PictureRepository extends JpaRepository<Picture, Long> {
-
-}
